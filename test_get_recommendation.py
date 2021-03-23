208695a502f0256af96811697baf1987769e4fe6
Wassouf289 <mahmoudwassouf@gmail.com>
2021-03-24 00:15:47 +0100

    update postgres in db connection to: postgresql

--- test_get_recommendation.py
+++ test_get_recommendation.py
@@ -1,20 +1,20 @@
 import numpy as np
 import pandas as pd
-#from sqlalchemy import create_engine
+from sqlalchemy import create_engine
 from helper_functions import movieTitle_to_id
 from NMF_recommend import recommend_NMF
 from userBased_filtering_recommend import user_based_filtering_recommend
 from recommend_movies import Movie_Recommender
 
 movies_num = 5
-""" 
+
 #create connection to movies db
 HOST = 'localhost'
 PORT = '5432'
 DBNAME = 'movies_db'
-connection_string = f'postgres://{HOST}:{PORT}/{DBNAME}'
+connection_string = f'postgresql://{HOST}:{PORT}/{DBNAME}'
 db = create_engine(connection_string)
- """
+
 user_movies = ['Titanic','Afterglow','Forgetting Sarah Marshall','Bella','Heat']
 user_ratings = ['5.0','5.0','4.0','4.0','5.0']
 
@@ -37,11 +37,10 @@ def test_get_recommendation():
     assert len(recommended_movies) == movies_num 
 
 
-""" 
+
 def test_recommended_movies():
     ''' test if the recommended movies are valid ones from DB'''
 
     for movie in recommended_movies:
         movie_id = f"SELECT 'movieId' FROM movies_ratings WHERE title ='{movie}';"
         assert movie_id != None
- """
