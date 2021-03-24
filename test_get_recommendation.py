f3b5cf0bf6d2b7922519b105ad4d388d31dda344
Wassouf289 <mahmoudwassouf@gmail.com>
2021-03-24 16:11:47 +0100

    update test recommendation pytest

--- test_get_recommendation.py
+++ test_get_recommendation.py
@@ -5,21 +5,14 @@ from helper_functions import movieTitle_to_id
 from NMF_recommend import recommend_NMF
 from userBased_filtering_recommend import user_based_filtering_recommend
 from recommend_movies import Movie_Recommender
+from data_preprocessing import movies_ratings
 
 movies_num = 5
-
-#create connection to movies db
-HOST = 'localhost'
-PORT = '5432'
-DBNAME = 'movies_db'
-connection_string = f'postgresql://{HOST}:{PORT}/{DBNAME}'
-db = create_engine(connection_string)
 
 user_movies = ['Titanic','Afterglow','Forgetting Sarah Marshall','Bella','Heat']
 user_ratings = ['5.0','5.0','4.0','4.0','5.0']
 
 Movie_Recommender = Movie_Recommender(user_movies,user_ratings)
-movies_ratings= Movie_Recommender.get_movies_ratings_data()
 user_movies_ids = movieTitle_to_id(user_movies,movies_ratings)
 new_user_vector = Movie_Recommender.new_user(user_movies_ids,user_ratings,movies_ratings)
 recommended_movies = recommend_NMF(new_user_vector,movies_num,movies_ratings)
@@ -37,10 +30,3 @@ def test_get_recommendation():
     assert len(recommended_movies) == movies_num 
 
 
-
-def test_recommended_movies():
-    ''' test if the recommended movies are valid ones from DB'''
-
-    for movie in recommended_movies:
-        movie_id = f"SELECT 'movieId' FROM movies_ratings WHERE title ='{movie}';"
-        assert movie_id != None
